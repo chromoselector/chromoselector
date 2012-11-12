7d8acc4915495dca5bbfd9c52bda8dece543846a
Rouslan Placella <rouslan@placella.com>
2012-11-12 22:56:50 +0000

    Rebuilt docs

---
+++ docs/demos/previews.js
@@ -0,0 +1,30 @@
+$(function () {
+
+    $('#inline').chromoselector({
+        preview: true
+    });
+
+    $('#no_preview').chromoselector({
+        preview: false
+    });
+
+    var updatePreview = function() {
+        var color = $(this).chromoselector('getColor');
+        $(this).css({
+            'background-color': color.hex,
+            'color': color.getTextColor().hex,
+            'text-shadow': '0 1px 0 ' + color.getTextColor().getTextColor().hex
+        });
+    };
+    $('#textfield').chromoselector({
+        preview: false,
+        create: updatePreview,
+        update: updatePreview
+    });
+
+    $(document).bind('updatelayout', function () {
+        $(this)
+            .find('#inline, #no_preview')
+            .chromoselector('reflow');
+    });
+});
