c2cfe903396d0e8ce35964f34d123664b680c8fd
Rouslan Placella <rouslan@placella.com>
2012-11-14 17:25:08 +0000

    More docs

---
+++ docs-src/overview/menu.php
@@ -0,0 +1,25 @@
+<?php
+
+function getOverviewMenu($path = '.', $selected = '') {
+    $html  = '<li data-role="list-divider">Overview</li>';
+    $elms = array(
+        'purchase' => 'Purchase',
+        'features' => 'Features',
+        'requirements' => 'Requirements',
+        'getting-started' => 'Getting Started',
+        'support' => 'Support'
+    );
+
+    foreach ($elms as $key => $value) {
+        if ($key == $selected) {
+            $html .= '<li data-theme="e">';
+        } else {
+            $html .= '<li>';
+        }
+        $html .= '<a href="' . $path . '/overview/' . $key . '.html">' . $value . '</a>';
+        $html .= '</li>';
+    }
+    return $html;
+}
+
+?>
