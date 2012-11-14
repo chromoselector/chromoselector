acca9c946f6db872e42a0676d24bdacd18246694
Rouslan Placella <rouslan@placella.com>
2012-11-14 17:25:22 +0000

    Rebuilt docs

--- docs/demos/display-modes.js
+++ docs/demos/display-modes.js
@@ -1,26 +1,14 @@
 $(function () {
     $('#basic1').chromoselector();
 
-    var updatePreview = function() {
-        var color = $(this).chromoselector('getColor');
-        $(this).css({
-            'background-color': color.hex,
-            'color': color.getTextColor().hex,
-            'text-shadow': '0 1px 0 ' + color.getTextColor().getTextColor().hex
-        });
-    };
-
     $('#basic2').chromoselector({
         target: '#picker',
         autoshow: false,
         create: function () {
-            updatePreview.call(this);
             $(this).chromoselector('show', 0);
         },
-        width: 260,
-        update: updatePreview
+        width: 260
     });
-    $('#picker').find('.ui-cs-container').removeClass('ui-cs-container');
     $('#picker').parent().append('<div style="clear: both;"></div>');
 
 
