2e900a3228ece3693706facde442a8fb9f2adbfe
Rouslan Placella <rouslan@placella.com>
2013-01-14 20:01:58 +0000

    Added copyright script

---
+++ copyright-notice.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function () {
+    // for regular demos
+    var $target = $('body');
+    // for jQuery mobile demo
+    if ($('div[data-role=page]').length > 0) {
+        $target = $('div[data-role=page]');
+    }
+    // append notice to page
+    $target.append(
+        $('<div/>').css({
+            'font-size':'0.7em',
+            'padding':'2.5em 0.7em',
+            'clear':'both'
+        }).append(
+            $('<p/>').append(
+                'This demo uses the proprietary chromoselector jQuery plugin.'
+            )
+        ).append(
+            $('<p/>').append(
+                'Please visit <a href="http://www.chromoselector.com/" target="_blank">http://www.chromoselector.com/</a> to purchase a license.'
+            )
+        )
+    );
+});
