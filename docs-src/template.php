58086935bf27bd929905311bc2119121b9f18db9
Rouslan Placella <rouslan@placella.com>
2013-04-21 16:13:14 +0100

    Concatenated demo scripts

--- docs-src/template.php
+++ docs-src/template.php
@@ -136,12 +136,7 @@ $(window).load(function () {
     } else {
         $html .= '<script src="' . $libPath . '/chromoselector.demo.min.js" type="text/javascript"></script>';
     }
-    $html .= '<script src="' . $path . '/demos/custom-color-format.js" type="text/javascript"></script>';
-    $html .= '<script src="' . $path . '/demos/icon.js" type="text/javascript"></script>';
-    $html .= '<script src="' . $path . '/demos/showcase.js" type="text/javascript"></script>';
-    $html .= '<script src="' . $path . '/demos/previews.js" type="text/javascript"></script>';
-    $html .= '<script src="' . $path . '/demos/animations.js" type="text/javascript"></script>';
-    $html .= '<script src="' . $path . '/demos/display-modes.js" type="text/javascript"></script>';
+    $html .= '<script src="' . $path . '/demos/demos.js" type="text/javascript"></script>';
     $html .= '<script src="' . $path . '/libs/highlight.pack.js" type="text/javascript"></script>';
 
 
