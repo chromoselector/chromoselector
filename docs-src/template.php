1229d508509a69f07d69f5978bdd022517c6bf3e
Rouslan Placella <rouslan@placella.com>
2013-08-28 21:34:05 +0100

    Fixed lists in docs with IE9

--- docs-src/template.php
+++ docs-src/template.php
@@ -158,6 +158,27 @@ $(window).load(function () {
 
     $html .= '});';
     $html .= '</script>';
+
+    $html .= '<script type="text/javascript">
+    if (navigator.userAgent.match(/MSIE 10/)) {
+        $(document).bind("pageinit", function () {
+            var classes = "abcde".split("");
+            for (var i in classes) {
+                $(".ui-btn-up-"+classes[i])
+                    .removeClass("ui-btn-up-"+classes[i])
+                    .addClass("backup-ui-btn-up-"+classes[i]);
+            }
+            setTimeout(function () {
+                for (var i in classes) {
+                    $(".backup-ui-btn-up-"+classes[i])
+                        .addClass("ui-btn-up-"+classes[i]);
+                }
+
+            }, 40)
+        });
+    }';
+    $html .= '</script>';
+
     $html .= '</body></html>';
     return $html;
 }
