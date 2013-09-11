53246b0b1f81c956733e53537dc3c58798a496c8
Rouslan Placella <rouslan@placella.com>
2013-09-11 12:44:51 +0100

    Fixed build script

--- chromoselector/makedocs.sh
+++ chromoselector/makedocs.sh
@@ -14,12 +14,12 @@ rm docs/screenshots/*
 cp -R docs-src/screenshots/* docs/screenshots/
 
 rm -f docs/*.html
-wget -q -O docs/index.html http://localhost/chromoselector/docs-src/index.html$ARGS
+wget -q -O docs/index.html http://localhost/chromoselector/chromoselector/docs-src/index.html$ARGS
 
 rm -f docs/api/*.html
 list=$(cd docs-src/api && ls *.html);
 for file in $list; do
-  wget -q -O docs/api/$file http://localhost/chromoselector/docs-src/api/$file$ARGS;
+  wget -q -O docs/api/$file http://localhost/chromoselector/chromoselector/docs-src/api/$file$ARGS;
 done
 
 rm -f docs/demos/*
@@ -27,11 +27,11 @@ cp docs-src/demos/*.js docs/demos/
 cp docs-src/api/*.png docs/api/
 list=$(cd docs-src/demos && ls *.html);
 for file in $list; do
-  wget -q -O docs/demos/$file http://localhost/chromoselector/docs-src/demos/$file$ARGS;
+  wget -q -O docs/demos/$file http://localhost/chromoselector/chromoselector/docs-src/demos/$file$ARGS;
 done
 
 rm -f docs/overview/*.html
 list=$(cd docs-src/overview && ls *.html);
 for file in $list; do
-  wget -q -O docs/overview/$file http://localhost/chromoselector/docs-src/overview/$file$ARGS;
+  wget -q -O docs/overview/$file http://localhost/chromoselector/chromoselector/docs-src/overview/$file$ARGS;
 done
