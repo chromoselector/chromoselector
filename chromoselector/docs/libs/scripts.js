51100be0e867d8593904b4966b21a79a757cfd65
Rouslan Placella <rouslan@placella.com>
2014-02-22 20:43:34 +0000

    Rebuilt docs

--- chromoselector/docs/libs/scripts.js
+++ chromoselector/docs/libs/scripts.js
@@ -1,3 +1,20 @@
+$(window).load(function () {
+    var url = location.href;
+    var target = "";
+    var index = url.lastIndexOf("?target=");
+    if (index !== -1) {
+        target = url.substr(index + 8);
+    }
+    if (target) {
+        var $target = $("#" + target);
+        if ($target.length) {
+            $target.find("h3").click();
+            $("html, body").animate({
+                scrollTop: $("#" + target).find("h3").offset().top
+            }, 400);
+        }
+    }
+});
 $(document).bind("pageinit", function(){
     $("h3.ui-collapsible-heading").bind("click", function () {
         $(this).parent().find("pre code").not(".done").addClass("done").each(function(i, e) {
