65edd3e94a780192d1a292376ef239fb22f738d7
Rouslan Placella <rouslan@placella.com>
2014-03-18 15:24:33 +0000

    Updated preview module

--- module-preview/preview.js
+++ module-preview/preview.js
@@ -1,88 +1,79 @@
-var Preview = (function(previewColor, width, shadowColor, shadowRadius) {
-    var getShadowCss = function (shadowColor, shadowRadius) {
-        shadowColor = new Color(shadowColor)
-            .setAlpha(shadowCssColor.getAlpha() - 0.1)
-            .getRgbaString();
-        shadowRadius = Math.max(0, shadowRadius - 2);
-        return '0 0 ' + shadowRadius + 'px 0 ' + shadowColor;
-    };
-
-    var self = this;
-    var shadowCss = getShadowCss(shadowColor, shadowRadius);
-    var $root = $('<div/>')
-        .addClass('ui-cs-preview-container');
-    var $previewWidget = $('<div/>')
-        .addClass('ui-cs-preview-widget')
-        .css('overflow', 'hidden')
-        .css('box-shadow', shadowCss)
-        .css('-webkit-box-shadow', shadowCss);
-    var $previewCanvas = $('<canvas/>').css({ display:'block' });
-    var $previewColor = $('<div/>')
-        .addClass('ui-cs-preview-color')
-        .css('width','100%')
-        .css('position', 'relative');
-
-    $root.append(
-        $previewWidget
-            .append($previewCanvas)
-            .append($previewColor);
-    );
-
-    var setColor = function (color) {
-        $previewColor.css('background-color', color.getRgbaString());
-    };
-    var update = function (newWidth) {
-        var previewHeight = $previewWidget.height();
-        $previewCanvas.height(previewHeight);
-        $previewColor
-            .css('top', '-' + previewHeight + 'px')
-            .height(previewHeight);
-        var ctx = $previewCanvas[0].getContext('2d');
-        $previewCanvas[0].height = previewHeight;
-        $previewCanvas[0].width = 500;
-        $previewCanvas.css('width', '500px');
-        // Draw checkboard background
+var Preview = (function() {
+    var makePattern = function (ctx) {
         var tempCanvas = document.createElement('canvas');
         tempCanvas.height = 10;
         tempCanvas.width = 10;
         var tempCtx = tempCanvas.getContext('2d');
-        tempCtx[fillStyle] = '#ccc';
-        tempCtx[fillRect](0, 0, 10, 10);
-        tempCtx[fillStyle] = '#888';
-        tempCtx[fillRect](0, 0, 5, 5);
-        tempCtx[fillRect](5, 5, 5, 5);
-        var pattern = ctx.createPattern(tempCanvas, 'repeat');
-        ctx[fillStyle] = pattern;
-        ctx[fillRect](
-            0,
-            0,
-            newWidth,
-            previewHeight
-        );
+        tempCtx['fillStyle'] = '#ccc';
+        tempCtx['fillRect'](0, 0, 10, 10);
+        tempCtx['fillStyle'] = '#888';
+        tempCtx['fillRect'](0, 0, 5, 5);
+        tempCtx['fillRect'](5, 5, 5, 5);
+        return ctx.createPattern(tempCanvas, 'repeat');
     };
-
-    update(width);
-    setColor(previewColor);
-
-    return {
+    var setColor = function (ctx, color, previewWidth, previewHeight) {
+        ctx['fillStyle'] = color.getRgbaString();
+        ctx['fillRect'](0, 0, previewWidth, previewHeight);
+    };
+    var drawPattern = function (ctx, pattern, previewWidth, previewHeight) {
+        ctx['fillStyle'] = pattern;
+        ctx['fillRect'](0, 0, previewWidth, previewHeight);
+    };
+    var setWidth = function ($target, $previewWidget, newWidth, previewWidth, previewHeight) {
+        previewWidth = newWidth - ($target.innerWidth() - $target.width());
+        $previewWidget.height(previewHeight);
+        $previewWidget[0].height = previewHeight;
+        $previewWidget[0].width = previewWidth;
+        $previewWidget.add($target).width(previewWidth).height(previewHeight);
+        return previewWidth;
+    };
+    var getShadowCss = function (shadowColor, shadowRadius) {
+        shadowColor = new Color(shadowColor)
+            .setAlpha(shadowColor.getAlpha() - 0.1)
+            .getRgbaString();
+        shadowRadius = Math.max(0, shadowRadius - 2);
+        return '0 0 ' + shadowRadius + 'px 0 ' + shadowColor;
+    };
+    return function($target, color, width, shadowColor, shadowRadius) {
+        var $previewWidget, previewColor, previewWidth, previewHeight, pattern, ctx;
+        var self = this;
+        var shadowCss = getShadowCss(shadowColor, shadowRadius);
+        $target.addClass('ui-cs-preview-container');
+        $previewWidget = $('<canvas/>')
+            .addClass('ui-cs-preview-widget')
+            .css({
+                'box-shadow': shadowCss,
+                '-webkit-box-shadow': shadowCss
+            });
+        $target.append(
+            $previewWidget
+        );
+        ctx = $previewWidget[0].getContext('2d');
+        pattern = makePattern(ctx);
+        previewHeight = $target.height();
+        previewWidth = setWidth($target, $previewWidget, width, previewWidth, previewHeight);
+        drawPattern(ctx, pattern, previewWidth, previewHeight);
+        previewColor = color;
+        setColor(ctx, color, previewWidth, previewHeight);
         // public functions
-        setColor: function (color) {
-            setColor(color);
-        },
-        update: function () {
-            update();
-        },
-        getHeight: function () {
-            return $root.outerHeight();
-        },
-        getWidth: function () {
-            return width;
-        },
-        setWidth: function (newWidth) {
-            update(newWidth);
-        },
-        getElement: function () {
-            return $root;
-        }
+        self.setColor = function (color) {
+            if (color.getAlpha() < 1) {
+                drawPattern(ctx, pattern, previewWidth, previewHeight);
+            }
+            previewColor = color;
+            setColor(ctx, color, previewWidth, previewHeight);
+        };
+        self.setWidth = function (newWidth) {
+            previewHeight = $target.height();
+            previewWidth = setWidth($target, $previewWidget, newWidth, previewWidth, previewHeight);
+            drawPattern(ctx, pattern, previewWidth, previewHeight);
+            setColor(ctx, previewColor, previewWidth, previewHeight);
+        };
+        self.getHeight = function () {
+            return $target.outerHeight(true);
+        };
+        self.getWidth = function () {
+            return $target.outerWidth(true);
+        };
     };
 })();
